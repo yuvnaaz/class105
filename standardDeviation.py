77b19d418bbde3baddca704091af3400305a2499
yuvnaaz <69728010+yuvnaaz@users.noreply.github.com>
2021-07-22 10:44:59 -0700

    Add files via upload

---
+++ standardDeviation.py
@@ -0,0 +1,28 @@
+import csv 
+import math
+with open('data.csv', newline = '') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+data = file_data[0]
+#finding mean
+def mean(data):
+    n = len(data)
+    total = 0
+    for x in data:
+        total += int(x)
+    mean = total/n
+    return mean
+squared_list = []
+for number in data:
+    a = int(number) - mean(data)
+    a = a**2
+    squared_list.append(a)
+sum = 0
+for i in squared_list:
+    sum = sum +i 
+result = sum/(len(data) -1)
+standardDeviation = math.sqrt(result)
+print(standardDeviation)
+
+
+
